7f4ee2d1cd8c717b1a43dbe3f179e6b36b387079
sugatiii <ahmadfahmisugati@gmail.com>
2025-01-10 23:36:53 +0700

    first commit

--- api/main.go
+++ api/main.go
@@ -5,13 +5,9 @@ import (
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+// Function handler yang akan diekspos ke Vercel
+func Handler(w http.ResponseWriter, r *http.Request) {
 	response := map[string]string{"message": "Hello from Golang API on Vercel!"}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
-}
-
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":3000", nil)
 }
